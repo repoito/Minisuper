dd947cdc8bcd47040b97dd675b46b579eee91ce7
Darkar <110624921+Blayker@users.noreply.github.com>
2023-06-19 22:52:24 -0600

    Delete ProyectoFinalAPI directory

--- ProyectoFinalAPI/Repositories/IRepository/IEmpleadoRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using ProyectoFinalAPI.Models;
-
-namespace ProyectoFinalAPI.Repositories.IRepository
-{
-    public interface IEmpleadoRepository : IRepository<Empleadoo>
-    {
-        Task<Empleadoo> GetEmpleadoo(int Id, string Nombre);
-    }
-}
--- ProyectoFinalAPI/Repositories/IRepository/IProductoRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using ProyectoFinalAPI.Models;
-
-namespace ProyectoFinalAPI.Repositories.IRepository
-{
-    public interface IProductoRepository : IRepository<Producto>
-    {
-        Task<Producto> Update(Producto entity);
-    }
-}
