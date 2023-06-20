576909afb6c904bb9272940a57f95bec44fcc53c
50587 <50587@LAPTOP-O6TEK9N4>
2023-06-19 20:10:29 -0600

    Agregar archivos de proyecto.

---
+++ pjHiperShop/frmHipershop.Designer.cs
@@ -0,0 +1,317 @@
+﻿namespace pjHiperShop
+{
+    partial class frmHipershop
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmHipershop));
+            this.Barratitulo = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Btncerrar = new System.Windows.Forms.PictureBox();
+            this.MenuVertical = new System.Windows.Forms.Panel();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.btnventas = new System.Windows.Forms.Button();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.btnempleados = new System.Windows.Forms.Button();
+            this.btncompra = new System.Windows.Forms.Button();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.btninventario = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Barratitulo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Btncerrar)).BeginInit();
+            this.MenuVertical.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Barratitulo
+            // 
+            this.Barratitulo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(80)))), ((int)(((byte)(200)))));
+            this.Barratitulo.Controls.Add(this.pictureBox2);
+            this.Barratitulo.Controls.Add(this.pictureBox1);
+            this.Barratitulo.Controls.Add(this.Btncerrar);
+            this.Barratitulo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.Barratitulo.Location = new System.Drawing.Point(0, 0);
+            this.Barratitulo.Name = "Barratitulo";
+            this.Barratitulo.Size = new System.Drawing.Size(1300, 35);
+            this.Barratitulo.TabIndex = 0;
+            this.Barratitulo.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Barratitulo_MouseDown);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(1210, 7);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(25, 25);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(1241, 7);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(25, 25);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Btncerrar
+            // 
+            this.Btncerrar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Btncerrar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Btncerrar.Image = global::pjHiperShop.Properties.Resources.th;
+            this.Btncerrar.Location = new System.Drawing.Point(1272, 7);
+            this.Btncerrar.Name = "Btncerrar";
+            this.Btncerrar.Size = new System.Drawing.Size(25, 25);
+            this.Btncerrar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Btncerrar.TabIndex = 0;
+            this.Btncerrar.TabStop = false;
+            this.Btncerrar.Click += new System.EventHandler(this.Btncerrar_Click);
+            // 
+            // MenuVertical
+            // 
+            this.MenuVertical.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(32)))), ((int)(((byte)(40)))));
+            this.MenuVertical.Controls.Add(this.pictureBox8);
+            this.MenuVertical.Controls.Add(this.pictureBox7);
+            this.MenuVertical.Controls.Add(this.btnventas);
+            this.MenuVertical.Controls.Add(this.pictureBox6);
+            this.MenuVertical.Controls.Add(this.pictureBox5);
+            this.MenuVertical.Controls.Add(this.btnempleados);
+            this.MenuVertical.Controls.Add(this.btncompra);
+            this.MenuVertical.Controls.Add(this.pictureBox4);
+            this.MenuVertical.Controls.Add(this.btninventario);
+            this.MenuVertical.Controls.Add(this.pictureBox3);
+            this.MenuVertical.Dock = System.Windows.Forms.DockStyle.Left;
+            this.MenuVertical.Location = new System.Drawing.Point(0, 35);
+            this.MenuVertical.Name = "MenuVertical";
+            this.MenuVertical.Size = new System.Drawing.Size(200, 615);
+            this.MenuVertical.TabIndex = 1;
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox8.Image")));
+            this.pictureBox8.Location = new System.Drawing.Point(3, 587);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(25, 25);
+            this.pictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox8.TabIndex = 9;
+            this.pictureBox8.TabStop = false;
+            this.pictureBox8.Click += new System.EventHandler(this.pictureBox8_Click);
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox7.Image")));
+            this.pictureBox7.Location = new System.Drawing.Point(12, 354);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(25, 25);
+            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox7.TabIndex = 8;
+            this.pictureBox7.TabStop = false;
+            // 
+            // btnventas
+            // 
+            this.btnventas.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(32)))), ((int)(((byte)(40)))));
+            this.btnventas.FlatAppearance.BorderSize = 0;
+            this.btnventas.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(80)))), ((int)(((byte)(200)))));
+            this.btnventas.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnventas.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnventas.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnventas.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnventas.Location = new System.Drawing.Point(3, 346);
+            this.btnventas.Name = "btnventas";
+            this.btnventas.Size = new System.Drawing.Size(197, 33);
+            this.btnventas.TabIndex = 7;
+            this.btnventas.Text = "Ventas";
+            this.btnventas.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox6.Image")));
+            this.pictureBox6.Location = new System.Drawing.Point(12, 231);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(25, 25);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox6.TabIndex = 6;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
+            this.pictureBox5.Location = new System.Drawing.Point(12, 287);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(25, 25);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox5.TabIndex = 5;
+            this.pictureBox5.TabStop = false;
+            // 
+            // btnempleados
+            // 
+            this.btnempleados.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(32)))), ((int)(((byte)(40)))));
+            this.btnempleados.FlatAppearance.BorderSize = 0;
+            this.btnempleados.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(80)))), ((int)(((byte)(200)))));
+            this.btnempleados.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnempleados.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnempleados.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnempleados.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnempleados.Location = new System.Drawing.Point(2, 287);
+            this.btnempleados.Name = "btnempleados";
+            this.btnempleados.Size = new System.Drawing.Size(197, 33);
+            this.btnempleados.TabIndex = 4;
+            this.btnempleados.Text = "Empleados";
+            this.btnempleados.UseVisualStyleBackColor = false;
+            // 
+            // btncompra
+            // 
+            this.btncompra.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(32)))), ((int)(((byte)(40)))));
+            this.btncompra.FlatAppearance.BorderSize = 0;
+            this.btncompra.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(80)))), ((int)(((byte)(200)))));
+            this.btncompra.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btncompra.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btncompra.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btncompra.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btncompra.Location = new System.Drawing.Point(3, 231);
+            this.btncompra.Name = "btncompra";
+            this.btncompra.Size = new System.Drawing.Size(197, 25);
+            this.btncompra.TabIndex = 3;
+            this.btncompra.Text = "Compra";
+            this.btncompra.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::pjHiperShop.Properties.Resources.R__1_;
+            this.pictureBox4.Location = new System.Drawing.Point(12, 159);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(25, 25);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox4.TabIndex = 2;
+            this.pictureBox4.TabStop = false;
+            // 
+            // btninventario
+            // 
+            this.btninventario.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(32)))), ((int)(((byte)(40)))));
+            this.btninventario.FlatAppearance.BorderSize = 0;
+            this.btninventario.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(80)))), ((int)(((byte)(200)))));
+            this.btninventario.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btninventario.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btninventario.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btninventario.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btninventario.Location = new System.Drawing.Point(0, 159);
+            this.btninventario.Name = "btninventario";
+            this.btninventario.Size = new System.Drawing.Size(197, 25);
+            this.btninventario.TabIndex = 1;
+            this.btninventario.Text = "Inventario";
+            this.btninventario.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(200, 124);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 0;
+            this.pictureBox3.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(66)))), ((int)(((byte)(82)))));
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(200, 35);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1100, 615);
+            this.panel1.TabIndex = 2;
+            // 
+            // frmHipershop
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1300, 650);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.MenuVertical);
+            this.Controls.Add(this.Barratitulo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(1);
+            this.Name = "frmHipershop";
+            this.Text = "frmHipershop";
+            this.Barratitulo.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Btncerrar)).EndInit();
+            this.MenuVertical.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        private void Barratitulo_MouseDown(object sender, MouseEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        private Panel Barratitulo;
+        private PictureBox Btncerrar;
+        private Panel MenuVertical;
+        private Panel panel1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox1;
+        private Button btninventario;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox6;
+        private PictureBox pictureBox5;
+        private Button btnempleados;
+        private Button btncompra;
+        private PictureBox pictureBox7;
+        private Button btnventas;
+        private PictureBox pictureBox8;
+    }
+}
---
+++ pjHiperShop/frmHipershop.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace pjHiperShop
+{
+    public partial class frmHipershop : Form
+    {
+        public frmHipershop()
+        {
+            InitializeComponent();
+        }
+
+        private void Btncerrar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            this.WindowState= FormWindowState.Minimized;
+            pictureBox2.Visible= false;
+            pictureBox2.Visible = true;
+        }
+        
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized;
+            pictureBox1.Visible = false;
+            pictureBox1.Visible = true;
+
+
+
+        }
+
+        private void pictureBox8_Click(object sender, EventArgs e)
+        {
+
+            Application.Exit();
+      
+        }
+
+       /* private void Barratitulo_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }*/
+    }
+}
